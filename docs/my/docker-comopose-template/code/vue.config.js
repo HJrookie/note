ba5692687cfaa7fc26ecbf80effa9b4d9b060410
rookie <1181400768@qq.com>
2022-09-07 10:10:51 +0800

    try to add search

--- docs/my/docker-comopose-template/code/vue.config.js
+++ docs/my/docker-comopose-template/code/vue.config.js
@@ -1,10 +1,11 @@
+//     "copy-webpack-plugin": "^6.1.0",
+// "@vue/cli-plugin-babel": "4.4.4",
+// "@vue/cli-plugin-eslint": "^3.9.1",
+// "@vue/cli-service": "4.4.4",
 "use strict";
 const path = require("path");
-const defaultSettings = require("./src/settings.js");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const name = defaultSettings.title || ""; // page title
-// All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
     chainWebpack(config) {
         config.when(process.env.NODE_ENV !== "development", (config) => {
