400f22e807c072a9dadf7a13a6388e2df601e2e1
rookie <1181400768@qq.com>
2022-10-15 17:03:08 +0800

    update thunkify , add todo

--- docs/front/ecma/js/thunkify.js
+++ docs/front/ecma/js/thunkify.js
@@ -11,16 +11,16 @@ var gen = function* () {
 const autoRunner = (func) => {
     const g = func();
     function next(err, data) {
-        console.log('err --> ', err, data,)
+        console.log("err --> ", err, data);
         const res = g.next(data);
-        console.log('res -->', res)
+        console.log("res -->", res);
         if (res.done) return;
-        res.value(next)
+        res.value(next);  // value 是一个函数
     }
     next();
 };
 
-autoRunner(gen)
+// autoRunner(gen)
 
 /* 
 err -->  undefined undefined
@@ -32,3 +32,26 @@ err -->  null <Buffer 32 32 32 32 32 32 32 32 32 32>
 2222222222
 res --> { value: undefined, done: true }
 */
+
+
+
+// 1. 例1
+// const cb = (err, data) => {
+//     console.log(111);
+// };
+// readFileThunk("./1.txt")(cb); // 一般用法,多个参数分别多次调用,最后是回调  
+
+// 1. 例2
+function* test2() {
+    var r1 = yield readFileThunk("./1.txt");
+    console.log(r1.toString());
+}
+const gen2 = test2();
+
+let a = gen2.next();
+console.log(a,) // 执行到了读取 1.txt,返回的 value是一个函数,可以接收回调
+let b = gen2.next(444); // 444 这个参数会作为r1的值,并且被打印
+console.log(b)
+// { value: [Function (anonymous)], done: false }
+// 444
+// { value: undefined, done: true }
