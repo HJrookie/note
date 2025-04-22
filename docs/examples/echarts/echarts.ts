5a4f79450c5cb20cf57c62c2a5dc324a913a8ece
sunSun <1181400768@qq.com>
2025-04-22 15:13:15 +0800

    Update echarts.ts

--- docs/examples/echarts/echarts.ts
+++ docs/examples/echarts/echarts.ts
@@ -1,3 +1,68 @@
+// 两条折线图
+const getOption = (allData = {}, color = ["#00DCEC", "#1890FF", "#EF6D6C", "#8E42FF", "#13C2C2", "#F7B500"]) => {
+  const option = {
+    tooltip: {
+      trigger: "axis",
+      // axisPointer: { type: 'cross' }
+      // formatter: function (params) {
+      //   // 时间
+      //   let tiem1 = convertSeconds(+params[0].axisValue) + '<br/>';
+      //   let tiem2 = convertSeconds(+params[1].axisValue) + '<br/>';
+      //   // 人名 + 语速
+      //   const texts2 = params.map((item) => {
+      //     return item.marker + `${item.seriesName}:` + (item.value);
+      //   });
+
+      //   return tiem1 + texts2[0] + '<br/>' + tiem2 + texts2[1];
+      // }
+    },
+    xAxis: {
+      type: "category",
+      data: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+    },
+    yAxis: [
+      {
+        type: "value",
+        name: "降水量",
+        min: 0,
+        max: 250,
+        // position: 'right',
+        axisLabel: {
+          formatter: "{value} ml",
+        },
+      },
+      {
+        type: "value",
+        name: "温度",
+        min: 0,
+        max: 25,
+        // position: 'left',
+        axisLabel: {
+          formatter: "{value} °C",
+        },
+      },
+    ],
+    series: [
+      {
+        name: "降水量",
+        type: "line",
+        yAxisIndex: 0,
+        data: [6, 32, 70, 86, 68.7, 100.7, 125.6, 112.2, 44, 22, 77, 11],
+      },
+      {
+        name: "温度",
+        type: "line",
+        smooth: true,
+        yAxisIndex: 1,
+        data: [6.0, 10.2, 10.3, 11.5, 10.3, 13.2, 14.3, 16.4, 18.0, 16.5, 12.0, 5.2],
+      },
+    ],
+  };
+  return option;
+};
+
+
+
 // 正常  显示的柱状图
 export const getSaleOption = (xAxisData: any[], seriesData: any[], activeIndex: number) => {
     return {
